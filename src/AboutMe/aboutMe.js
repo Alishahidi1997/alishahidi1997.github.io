76b361e92c74c1912ce2ca2b6b383bc0ac2e36de
Ali <shahidi.ali.1997@gmail.com>
2024-02-20 12:42:38 -0700

    add bio

--- src/AboutMe/aboutMe.js
+++ src/AboutMe/aboutMe.js
@@ -6,7 +6,7 @@ function showPdf(){
 }
 function AboutMe(){
     const pdfPath = "../Assets/AliShahidiResume.pdf";
-    const summaryAboutMe = "Dedicated grad research/teaching assistant at the University of Calgary, pursuing a master's in computer software engineering. Leading an AR education app project for non-speaking autistic individuals. With 3+ years in Unity and C#, and a Udemy certification, I'm proficient in C#, AR, Unity 3D, Python, and game development.\
+    const summaryAboutMe = "I am a dedicated grad research/teaching assistant at the University of Calgary, pursuing a master's in computer software engineering. Leading an AR education app project for non-speaking autistic individuals. With 3+ years in Unity and C#, and a Udemy certification, I'm proficient in C#, AR, Unity 3D, Python, and game development.\
     Having recently completed \"The Complete 2023 Web Development Bootcamp,\" I've acquired an extensive skill set encompassing Front-End technologies such as HTML, CSS, JavaScript, and React.js, alongside Backend proficiency in Node.js, Express.js, and PostgreSQL.\
     My training has equipped me to excel in both web development and game development, offering expertise in building dynamic and scalable applications. As I pursue opportunities, I am eager to contribute my robust skills to innovative projects and bring valuable insights to the team."
     return(
